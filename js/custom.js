89137e736eb73af535352471c52bb17f45966f2d
imranH666 <hasanemran743@gmail.com>
2023-11-11 21:47:41 +0600

    Second commit

---
+++ js/custom.js
@@ -0,0 +1,81 @@
+
+$('.banner-main').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  
+
+  $('.screenshort-main').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {   
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  });
+
+
+  $('.feed-text-box-main').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.feed-profile'
+  });
+  $('.feed-profile').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    asNavFor: '.feed-text-box-main',
+    centerMode: true,
+    centerPadding: '10px',
+    responsive: [
+      {   
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+  
+
+
+
+
+
+
+
+
+  
